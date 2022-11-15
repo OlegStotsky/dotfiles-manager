f8fcc758f8b580e8448e5a8aeeb8b7440f2dc61b
Oleg Stotsky <ostotsky@gmail.com>
2022-11-15 18:20:53 +0400

    remove alfred, add raycast

--- pkg/registry/desktop.go
+++ pkg/registry/desktop.go
@@ -7,3 +7,9 @@ var Alfred = register(dotfiles.Component{
 	IsInstalled: dotfiles.OsxAppExists("alfred"),
 	Install:     dotfiles.DmgInstall("https://cachefly.alfredapp.com/Alfred_5.0.5_2096.dmg", "Alfred", "Alfred 5"),
 })
+
+var Raycast = register(dotfiles.Component{
+	Name:        "raycast",
+	IsInstalled: dotfiles.OsxAppExists("raycast"),
+	Install:     dotfiles.DmgInstall("https://www.raycast.com/download", "Raycast", "Raycast"),
+})
